b76944f1d6ee5212a6f375909e6e5c63994069b6
Omar Yamany <omarmyamany@gmail.com>
2023-06-18 18:40:18 +0300

    Add LED Driver

--- STM32F103C8T6_Drivers/Src/main.c
+++ STM32F103C8T6_Drivers/Src/main.c
@@ -7,39 +7,39 @@
 /* GitHub        : https://github.com/Piistachyoo             		     */
 /*************************************************************************/
 
-#include "gpio_driver.h"
-#include "EXTI_driver.h"
-#include "lcd_driver.h"
+#include "led_driver.h"
 
-uint8 IRQ_Flag;
+#define delay_duration	1500
 
 void clock_init();
-void MyISR_EXTIB9(void);
-void my_wait(int x){
-	uint16 i, j;
-	for(i = 0; i < x; i++){
-		for(j = 0; j < 255; j++);
-	}
-}
+void my_wait(int x);
 
 int main(void)
 {
 	clock_init();
-	LCD_Init();
-	EXTI_PinConfig_t myEXTI;
-	myEXTI.EXTI_PIN = EXTI9PB9;
-	myEXTI.Trigger_Case = EXTI_TRIGGER_FALLING;
-	myEXTI.IRQ_EN = EXTI_IRQ_ENABLE;
-	myEXTI.P_IRQ_CallBack = MyISR_EXTIB9;
-	MCAL_EXTI_GPIO_Init(&myEXTI);
-	LCD_Send_string_Pos((uint8*)"Waiting EXTI!", LCD_FIRST_ROW, 1);
+	LED_cfg_t LEDA0;
+	LED_cfg_t LEDB0;
+	LEDA0.LED_Port = GPIOA;
+	LEDA0.LED_Pin.GPIO_PinNumber = GPIO_PIN_0;
+	LEDA0.LED_Pin.GPIO_MODE = GPIO_MODE_OUTPUT_PP;
+	LEDA0.LED_Pin.GPIO_OUTPUT_SPEED = GPIO_SPEED_10M;
+	LEDA0.LED_Mode = LED_Active_High;
+	LEDB0.LED_Port = GPIOB;
+	LEDB0.LED_Pin.GPIO_PinNumber = GPIO_PIN_0;
+	LEDB0.LED_Pin.GPIO_MODE = GPIO_MODE_OUTPUT_PP;
+	LEDB0.LED_Pin.GPIO_OUTPUT_SPEED = GPIO_SPEED_10M;
+	LEDB0.LED_Mode = LED_Active_High;
+	LED_Init(&LEDA0);
+	LED_Init(&LEDB0);
 	while(1){
-		if(IRQ_Flag){
-			LCD_Send_string_Pos((uint8*)"                ", LCD_SECOND_ROW, 1);
-			IRQ_Flag--;
-		}
-
-//		my_wait(350);
+		LED_Toggle(&LEDA0);
+		my_wait(delay_duration);
+		LED_Toggle(&LEDB0);
+		my_wait(delay_duration);
+		LED_Toggle(&LEDA0);
+		my_wait(delay_duration);
+		LED_Toggle(&LEDB0);
+		my_wait(delay_duration);
 	}
 }
 
@@ -49,9 +49,9 @@ void clock_init(){
 	RCC_AFIO_CLK_EN();
 }
 
-void MyISR_EXTIB9(void){
-	LCD_Send_string_Pos((uint8*)"EXTI PB9 Occured", LCD_SECOND_ROW, 1);
-	IRQ_Flag++;
-	my_wait(2000);
-
+void my_wait(int x){
+	uint16 i, j;
+	for(i = 0; i < x; i++){
+		for(j = 0; j < 255; j++);
+	}
 }
