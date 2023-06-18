b5e21873d8d7a9cf9303c51ae8461e010a13c73b
Omar Yamany <omarmyamany@gmail.com>
2023-06-18 20:17:59 +0300

    Upload Seven Segment driver

---
+++ STM32F103C8T6_Drivers/HAL/Inc/ss_driver.h
@@ -0,0 +1,85 @@
+/*************************************************************************/
+/* Author        : Omar Yamany                                    		 */
+/* Project       : STM32F103C8T6_Drivers  	                             */
+/* File          : ss_driver.h 			                            	 */
+/* Date          : Jun 18, 2023                                          */
+/* Version       : V1                                                    */
+/* GitHub        : https://github.com/Piistachyoo             		     */
+/*************************************************************************/
+#ifndef INC_SS_DRIVER_H_
+#define INC_SS_DRIVER_H_
+
+//----------------------------------------------
+// Section: Includes
+//----------------------------------------------
+#include "gpio_driver.h"
+
+//----------------------------------------------
+// Section: User type definitions
+//----------------------------------------------
+typedef enum{
+	SS_Common_Anode,
+	SS_Common_Cathode,
+	SS_Active_Mode_max
+}SS_Active_Mode_t;
+
+typedef struct{
+	GPIO_TypeDef *PORT;
+	GPIO_PinConfig_t PIN;
+}SS_PIN_t;
+
+typedef struct{
+	SS_PIN_t PINS[8];
+	SS_Active_Mode_t Active_Mode;
+}SS_cfg_t;
+
+//----------------------------------------------
+// Section: Macros Configuration References
+//----------------------------------------------
+
+// @ref SS_Dot_Status_define
+#define SS_DOT_ON	1
+#define SS_DOT_OFF	0
+
+// @ref SS_Numbers_define
+/* hexadecimal representation for common cathode seven segment */
+#define SS_ZERO		0x3FU
+#define SS_ONE		0x06U
+#define SS_TWO		0x5BU
+#define SS_THREE	0x4FU
+#define SS_FOUR		0x66U
+#define SS_FIVE		0x6DU
+#define SS_SIX		0x7DU
+#define SS_SEVEN	0x07U
+#define SS_EIGHT	0x7FU
+#define SS_NINE		0x6FU
+
+/*
+ * =============================================
+ * APIs Supported by "Seven Segment"
+ * =============================================
+ */
+
+/**=============================================
+  * @Fn				- SS_Init
+  * @brief 			- Initializes the seven segment's GPIO pins
+  * @param [in] 	- mySS: Pointer to the struct holding the seven segment configuration
+  * @param [out] 	- None
+  * @retval 		- None
+  * Note			- Seven segment initially shows no number
+  */
+void SS_Init(const SS_cfg_t *mySS);
+
+/**=============================================
+  * @Fn				- SS_Display_Number
+  * @brief 			- Shows corresponding number on
+  * @param [in] 	- mySS: Pointer to the struct holding the seven segment configuration
+  * @param [in] 	- number: The number that will be displayed on the seven segment @ref SS_Numbers_define
+  * @param [in] 	- dot_status: Defins the dot status if on or off @ref SS_Dot_Status_define
+  * @param [out] 	- None
+  * @retval 		- None
+  * Note			- Seven segment initially shows no number
+  */
+void SS_Display_Number(const SS_cfg_t *mySS, uint8 number, uint8 dot_status);
+
+#endif /* INC_SS_DRIVER_H_ */
---
+++ STM32F103C8T6_Drivers/HAL/ss_driver.c
@@ -0,0 +1,91 @@
+/*************************************************************************/
+/* Author        : Omar Yamany                                    		 */
+/* Project       : STM32F103C8T6_Drivers  	                             */
+/* File          : ss_driver.c 			                            	 */
+/* Date          : Jun 18, 2023                                          */
+/* Version       : V1                                                    */
+/* GitHub        : https://github.com/Piistachyoo             		     */
+/*************************************************************************/
+
+#include "ss_driver.h"
+
+/**=============================================
+  * @Fn				- SS_Init
+  * @brief 			- Initializes the seven segment's GPIO pins
+  * @param [in] 	- mySS: Pointer to the struct holding the seven segment configuration
+  * @param [out] 	- None
+  * @retval 		- None
+  * Note			- Seven segment initially shows no number
+  */
+void SS_Init(const SS_cfg_t *mySS){
+	uint8 index, SS_initial_state;
+	/* Validate that led_cfg is not a NULL pointer */
+	if(NULL != mySS){
+		/* Let initial state be OFF */
+		if(SS_Common_Anode == mySS->Active_Mode){
+			SS_initial_state = GPIO_PIN_SET;
+		}
+		else if(SS_Common_Cathode == mySS->Active_Mode){
+			SS_initial_state = GPIO_PIN_RESET;
+		}
+		else{
+			/* Default is common cathode */
+			SS_initial_state = GPIO_PIN_RESET;
+		}
+		/* Initialize GPIO PINS */
+		for(index = 0; index < 8; index++){
+			MCAL_GPIO_Init(mySS->PINS[index].PORT, (GPIO_PinConfig_t*)&(mySS->PINS[index].PIN));
+			MCAL_GPIO_WritePin(mySS->PINS[index].PORT, mySS->PINS[index].PIN.GPIO_PinNumber, SS_initial_state);
+		}
+	}
+	else{ /* Do Nothing */ }
+}
+
+/**=============================================
+  * @Fn				- SS_Display_Number
+  * @brief 			- Shows corresponding number on
+  * @param [in] 	- mySS: Pointer to the struct holding the seven segment configuration
+  * @param [in] 	- number: The number that will be displayed on the seven segment @ref SS_Numbers_define
+  * @param [in] 	- dot_status: Defins the dot status if on or off @ref SS_Dot_Status_define
+  * @param [out] 	- None
+  * @retval 		- None
+  * Note			- Seven segment initially shows no number
+  */
+void SS_Display_Number(const SS_cfg_t *mySS, uint8 number, uint8 dot_status){
+	uint8 index, SS_ON_mode, SS_OFF_mode;
+	/* Validate that led_cfg is not a NULL pointer */
+	if(NULL != mySS){
+		/* Check seven segment type */
+		if(SS_Common_Anode == mySS->Active_Mode){
+			SS_ON_mode = GPIO_PIN_RESET;
+			SS_OFF_mode = GPIO_PIN_SET;
+		}
+		else if(SS_Common_Cathode == mySS->Active_Mode){
+			SS_ON_mode = GPIO_PIN_SET;
+			SS_OFF_mode = GPIO_PIN_RESET;
+		}
+		else{
+			/* Default is common cathode */
+			SS_ON_mode = GPIO_PIN_SET;
+			SS_OFF_mode = GPIO_PIN_RESET;
+		}
+
+		/* Loop on number code */
+		for(index = 0; index < 7; index++){
+			if((number>>index)&0x01){
+				MCAL_GPIO_WritePin(mySS->PINS[index].PORT, mySS->PINS[index].PIN.GPIO_PinNumber, SS_ON_mode);
+			}
+			else{
+				MCAL_GPIO_WritePin(mySS->PINS[index].PORT, mySS->PINS[index].PIN.GPIO_PinNumber, SS_OFF_mode);
+			}
+		}
+		/* Check for dot status */
+		if(SS_DOT_ON == dot_status){
+			MCAL_GPIO_WritePin(mySS->PINS[7].PORT, mySS->PINS[7].PIN.GPIO_PinNumber, SS_ON_mode);
+		}
+		else{
+			MCAL_GPIO_WritePin(mySS->PINS[7].PORT, mySS->PINS[7].PIN.GPIO_PinNumber, SS_OFF_mode);
+		}
+	}
+	else{ /* Do Nothing */ }
+}
